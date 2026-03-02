72834c0f5d824545e3f2a20fc37138dc4da9e6fc
MuhammadAhdiyatul <muhammad.ahdiyatul.tif425@polban.ac.id>
2026-03-02 21:17:01 +0700

    menambahkanomor 1

---
+++ Soal1/TestCylinder.java
@@ -0,0 +1,29 @@
+package Soal1;
+
+public class TestCylinder { // save as "TestCylinder.java"
+    public static void main(String[] args) {
+        // Instans 1
+        Cylinder c1 = new Cylinder();
+        System.out.println("Cylinder 1:");
+        System.out.println(" radius=" + c1.getRadius() + ", height=" + c1.getHeight());
+        System.out.println(" surface area=" + c1.getArea()); // Memanggil overridden getArea()
+        System.out.println(" volume=" + c1.getVolume());
+        System.out.println(" " + c1.toString() + "\n"); // Menguji overridden toString()
+
+        // Instans 2
+        Cylinder c2 = new Cylinder(10.0);
+        System.out.println("Cylinder 2:");
+        System.out.println(" radius=" + c2.getRadius() + ", height=" + c2.getHeight());
+        System.out.println(" surface area=" + c2.getArea());
+        System.out.println(" volume=" + c2.getVolume());
+        System.out.println(" " + c2.toString() + "\n");
+
+        // Instans 3
+        Cylinder c3 = new Cylinder(2.0, 10.0);
+        System.out.println("Cylinder 3:");
+        System.out.println(" radius=" + c3.getRadius() + ", height=" + c3.getHeight());
+        System.out.println(" surface area=" + c3.getArea());
+        System.out.println(" volume=" + c3.getVolume());
+        System.out.println(" " + c3.toString());
+    }
+}
